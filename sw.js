36771900c40e66ed47fe2e3a766be83998104a12
J-AI-71 <goodonline24@gmail.com>
2025-11-12 15:58:58 +0100

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 // /Supersystem/sw.js
-const SW_VERSION = "2025-11-12"; // bei jedem Deploy erhöhen
-
+const SW_VERSION = "2025-11-12";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-// kein fetch-Handler -> kein aggressives Caching
+// kein fetch-Handler
