e276a5c6614715479e92b0050d207a463eb6f900
J-AI-71 <goodonline24@gmail.com>
2025-11-13 14:06:34 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – Version bei jedem Deploy erhöhen
-const SW_VERSION = "2025-11-13-06";
+const SW_VERSION = "2025-11-13-07";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 // kein fetch-Handler -> kein aggressives Caching
