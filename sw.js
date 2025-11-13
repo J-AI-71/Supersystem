a66ac640ce3adfa79a18ea3e6a1783a191b7f926
J-AI-71 <goodonline24@gmail.com>
2025-11-13 14:48:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,3 +3,4 @@ const SW_VERSION = "2025-11-13-07";
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
 // kein fetch-Handler -> kein aggressives Caching
+
