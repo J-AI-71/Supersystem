08e4f9cb4735bb0ab25af5875f8b02ed2e4c96d0
J-AI-71 <goodonline24@gmail.com>
2025-11-22 15:15:10 +0100

    Create page-quickstart.js

---
+++ js/page-quickstart.js
@@ -0,0 +1,7 @@
+/* /Supersystem/js/page-quickstart.js (v35) */
+(() => {
+  'use strict';
+  const btn = document.getElementById('do-print');
+  if (btn) btn.addEventListener('click', () => window.print());
+  console.log('[SafeShare] quickstart ready');
+})();
